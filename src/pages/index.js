8405cd5653dbc712c091e6a441cb1dc84315102e
Muhammad <usmanehsanullah3644@gmail.com>
2023-03-14 11:49:39 +0500

    fixes

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Banner from './Banner'
+import Services from './Services'
+import Site_design from './Site_design'
+import Banner_long from './Banner_long'
+import Service_offer from './Service_offer'
+import Contact from './Contact'
+import Slider from './Slider'
+
+
+
+
+const Index = () => {
+  return (
+    <>
+    
+    <Slider/>
+    <Banner/>
+    <Services/>
+    <Site_design/>
+    <Banner_long/>
+    <Service_offer/>
+    <Contact/>
+    
+
+   
+    </>
+  )
+}
+
+export default Index
